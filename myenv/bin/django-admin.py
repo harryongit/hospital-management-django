f4487d9e315d468ac7b61e37e6ce68d83bf7e340
shekru_labs <harryshastri21@gmail.com>
2024-07-10 23:03:18 +0530

    added code

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/Downloads/hostel_management_system_in_django/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
